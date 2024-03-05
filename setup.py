4082f56af39e0862091bfacb0770d4773571457f
Garth <244253+xgp@users.noreply.github.com>
2024-03-05 18:46:31 +0100

    update typing_extensions dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "phasetwo-sdk"
-VERSION = "0.1.1"
+VERSION = "0.2.0"
 # To install the library, run the following
 #
 # python setup.py install
@@ -25,7 +25,7 @@ REQUIRES = [
     "frozendict ~= 2.3.4",
     "python-dateutil ~= 2.7.0",
     "setuptools >= 21.0.0",
-    "typing_extensions ~= 4.3.0",
+    "typing_extensions >= 4.5.0",
     "urllib3 ~= 1.26.7",
 ]
 
